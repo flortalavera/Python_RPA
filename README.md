9146006c8ef19e889882e446ea957f99bb0355f5
flortalavera <florii.talavera@gmail.com>
2022-06-22 21:46:54 -0300

    mejoras en archivo readme, y continué el bonus track

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# challengeCalyx
+# Datos Argentina Museos - Cines - Bibliotecas populares
+
+## OBJETIVO
+Este proyecto entrega información cultural sobre bibliotecas, museos 
+y salas de cines argentinos.
+
+## PRE REQUISITOS
+• Python 3.6+
+• Pip
 
 ## EJECUCIÓN
-Desde el archivo app.py
+1. Clonar este repositorio
+$ https://github.com/flortalavera/challengeCalyx.git
+
+2. Instalar requerimientos (dentro de un ambiente virtual) 
+$ pip install -r requirements.txt
+
+3. Ejecutar script
+$ python main.py
+
+4. Como resultado obtendras:
 
 ## DEPLOY
+Heroku
--- bonus_track/automatitation.py
+++ bonus_track/automatitation.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import pandas as pd
+
+# WEB SCRAPING WITH SELENIUM
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -9,13 +12,27 @@ driver.maximize_window()
 time.sleep(5)
 
 quoteDate = driver.find_element_by_class_name("fechaCot").text
-
+print(quoteDate)
+print("==============================")
 thTableCot = driver.find_elements_by_xpath("//*[@id='billetes']/table/thead/tr")
 
 for th in thTableCot:
     print(th.text)
+print("==============================")
 
 trTableCot = driver.find_elements_by_xpath("//*[@id='billetes']/table/tbody/tr")
 
 for tr in trTableCot:
-    print(tr.text)
+    row = tr.text
+    print(row)
+print("==============================")
+
+# Saving data to a Dataframe
+
+quoteDataFrame = pd.DataFrame(columns=['Moneda', 'Compra', 'Venta', 'Promedio'],
+index=range(4))
+
+quoteDataFrame.iloc[0] = row
+
+
+print(quoteDataFrame)
