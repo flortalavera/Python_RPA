acf840c713617411ba5d18daf5830fd0ba671db2
flortalavera <florii.talavera@gmail.com>
2022-06-23 20:52:12 -0300

    conexion a base de datos exitosa, se visualizan las 3 tablas en la misma

--- create_tables/db.py
+++ create_tables/db.py
@@ -1,18 +1,9 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from requests import Session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from decouple import config
 
-# engine = config(connec)
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-Base = declarative_base()
-
-#subclase for constructing ours maps or models.
-
-class Message(Base):
-    __tableName__ = 'messages'
-
-    id = Column(Integer, primary_key=True)
-    message = Column(String)
+engine = create_engine("postgresql://cotzugyr:gNFad4ZCwP50TJG0uB_cMr5izbrqMoii@motty.db.elephantsql.com/cotzugyr")
+Base = declarative_base
+Session = sessionmaker(bind=engine)
+session = Session()
